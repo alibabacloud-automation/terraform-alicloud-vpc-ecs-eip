4f2311842a310513300c1b91fd6a0e75c3f50a0e
shanye <shanye.xzq@alibaba-inc.com>
2025-03-14 11:25:51 +0800

    examples/complete: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,7 +6,10 @@ data "alicloud_images" "default" {
 }
 
 data "alicloud_instance_types" "default" {
-  availability_zone = data.alicloud_zones.default.zones.0.id
+  availability_zone    = data.alicloud_zones.default.zones[0].id
+  cpu_core_count       = 2
+  memory_size          = 8
+  instance_type_family = "ecs.g6"
 }
 
 module "example" {
@@ -15,7 +18,7 @@ module "example" {
   # alicloud_vpc snd alicloud_vswitch
   name              = var.name
   cidr_block        = "172.16.0.0/18"
-  availability_zone = data.alicloud_zones.default.zones.0.id
+  availability_zone = data.alicloud_zones.default.zones[0].id
 
   # alicloud_eip_address
   eip_isp                  = "BGP"
@@ -30,11 +33,11 @@ module "example" {
   eip_instance_type = "NetworkInterface"
 
   # alicloud_instance
-  instance_type              = data.alicloud_instance_types.default.instance_types.0.id
+  instance_type              = data.alicloud_instance_types.default.instance_types[0].id
   system_disk_category       = "cloud_efficiency"
   system_disk_name           = var.system_disk_name
   system_disk_description    = var.system_disk_description
-  image_id                   = data.alicloud_images.default.images.0.id
+  image_id                   = data.alicloud_images.default.images[0].id
   internet_max_bandwidth_out = var.internet_max_bandwidth_out
   ecs_size                   = 1200
   data_disks_name            = "data_disks_name"
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,4 @@
 output "this_ecs_name" {
-  value = module.example.this_ecs_name
+  description = "The name of the ECS instance."
+  value       = module.example.this_ecs_name
 }
