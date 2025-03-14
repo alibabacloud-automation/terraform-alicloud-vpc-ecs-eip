fc71239564b9ac276a9403900f66cde81f6ca731
shanye <shanye.xzq@alibaba-inc.com>
2025-03-14 11:25:51 +0800

    examples/complete: improve the examples

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "this_ecs_id" {
-  value = alicloud_instance.default.id
+  description = "The ID of the ECS instance."
+  value       = alicloud_instance.default.id
 }
 
 output "this_vpc_id" {
-  value = alicloud_vpc.default.id
+  description = "The ID of the VPC."
+  value       = alicloud_vpc.default.id
 }
 
 output "this_alicloud_vswitch" {
-  value = alicloud_vswitch.default.id
+  description = "The ID of the VSwitch."
+  value       = alicloud_vswitch.default.id
 }
 
 output "this_eip_id" {
-  value = alicloud_eip_address.default.id
+  description = "The ID of the EIP."
+  value       = alicloud_eip_address.default.id
 }
 
 output "this_ecs_name" {
-  value = alicloud_instance.default.instance_name
+  description = "The name of the ECS instance."
+  value       = alicloud_instance.default.instance_name
 }
